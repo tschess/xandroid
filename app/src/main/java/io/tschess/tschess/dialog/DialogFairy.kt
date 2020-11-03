5469e8180800aaf694480d75d922df61de7ee127
smatthewenglish <s.matthew.english@gmail.com>
2020-11-03 09:55:19 -0500

    fairy adapter

--- app/src/main/java/io/tschess/tschess/dialog/DialogFairy.kt
+++ app/src/main/java/io/tschess/tschess/dialog/DialogFairy.kt
@@ -11,55 +11,42 @@ class DialogFairy(context: Context, val name: String) : Dialog(context) {
 
     lateinit var textTitle: TextView
     lateinit var imageFairy: ImageView
-    lateinit var textContentStrength: TextView
-    lateinit var textContentWeakness: TextView
+    lateinit var textContent: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dialog_fairy)
         this.textTitle = findViewById<TextView>(R.id.text_title)
         this.imageFairy = findViewById<ImageView>(R.id.image_fairy)
-        this.textContentStrength = findViewById<TextView>(R.id.content_strength)
-        this.textContentWeakness = findViewById<TextView>(R.id.content_weakness)
-        if(name.toLowerCase() == "poison"){
-            this.setPoison()
-        }
-        if(name.toLowerCase() == "hunter"){
-            this.setHunter()
-        }
-        if(name.toLowerCase() == "amazon"){
-            this.setAmazon()
-        }
+        this.textContent = findViewById<TextView>(R.id.content_text)
+        this.setContent(name, textContent)
     }
 
-    private fun setPoison() {
-        this.textTitle.text = "Poison Pawn"
-        this.imageFairy.setImageResource(R.drawable.red_landmine)
-        val contentStrength: String =
-            "• behaviour identical to a standard pawn with one caveat...\n• when captured, the opponent piece is also destroyed, i.e. eliminated from the game.\n• if king attempts to capture poison pawn the result is instant checkmate."
-        this.textContentStrength.text = contentStrength
-        val contentWeakness: String = "• appears to your opponent throughout the game as a standard pawn."
-        this.textContentWeakness.text = contentWeakness
-    }
+    private fun setContent(name: String, contentText: TextView) {
+        when (name.toLowerCase()) {
+            "hunter" -> {
+                this.textTitle.text = "Hunter"
+                this.imageFairy.setImageResource(R.drawable.red_hunter)
+                contentText.text = "• moves forward as standard bishop\n" +
+                        "• moves backwards as standard knight"
+                return
+            }
+            "poison" -> {
+                this.textTitle.text = "Poison Pawn"
+                this.imageFairy.setImageResource(R.drawable.red_landmine)
+                contentText.text =  "• appears to your opponent throughout the game as a standard pawn.\n" +
 
-    private fun setHunter() {
-        this.textTitle.text = "Hunter"
-        this.imageFairy.setImageResource(R.drawable.red_hunter)
-        val contentStrength: String =
-            "• behaviour identical to a standard pawn with one caveat...\n• when captured, the opponent piece is also destroyed, i.e. eliminated from the game.\n• if king attempts to capture poison pawn the result is instant checkmate."
-        this.textContentStrength.text = contentStrength
-        val contentWeakness: String = "• appears to your opponent throughout the game as a standard pawn."
-        this.textContentWeakness.text = contentWeakness
-    }
-
-    private fun setAmazon() {
+                        "• when captured, the opponent piece is also destroyed, i.e. eliminated from the game.\n" +
+                        "• if king attempts to capture poison pawn the result is instant checkmate.\n" +
+                        "• behaviour identical to a standard pawn\n" +
+                        "• cannot be promoted."
+                return
+            }
+        } //"amazon"
         this.textTitle.text = "Amazon"
         this.imageFairy.setImageResource(R.drawable.red_amazon)
-        val contentStrength: String =
-            "• behaviour identical to a standard pawn with one caveat...\n• when captured, the opponent piece is also destroyed, i.e. eliminated from the game.\n• if king attempts to capture poison pawn the result is instant checkmate."
-        this.textContentStrength.text = contentStrength
-        val contentWeakness: String = "• appears to your opponent throughout the game as a standard pawn."
-        this.textContentWeakness.text = contentWeakness
+        contentText.text = "• performs all behavior of standard queen\n" +
+                "• performs all behavior of standard knight"
     }
 
     companion object {
