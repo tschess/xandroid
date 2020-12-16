7667d7beef99bb6a20dd9c7233ddb05a508dd8f1
smatthewenglish <s.matthew.english@gmail.com>
2020-12-16 13:19:09 -0500

    fixeruper

--- app/src/main/java/io/tschess/tschess/start/ActivityStart.kt
+++ app/src/main/java/io/tschess/tschess/start/ActivityStart.kt
@@ -1,16 +1,32 @@
 package io.tschess.tschess.start
 
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.provider.Settings.Secure
+import android.util.Log
 import android.view.View
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.DefaultRetryPolicy
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
 import com.google.firebase.FirebaseApp
 import io.tschess.tschess.R
 import io.tschess.tschess.dialog.DialogOk
+import io.tschess.tschess.home.ActivityHome
+import io.tschess.tschess.model.EntityPlayer
+import io.tschess.tschess.model.ExtendedDataHolder
+import io.tschess.tschess.model.ParsePlayer
 import io.tschess.tschess.server.ServerAddress
+import io.tschess.tschess.server.VolleySingleton
+import org.json.JSONObject
 
 class ActivityStart : AppCompatActivity() {
 
@@ -22,6 +38,12 @@ class ActivityStart : AppCompatActivity() {
     private lateinit var buttonLogin: Button
     private lateinit var buttonCreate: Button
     private lateinit var buttonRecover: Button
+
+    val parsePlayer: ParsePlayer  = ParsePlayer()
+
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,7 +69,7 @@ class ActivityStart : AppCompatActivity() {
             val username: String = inputUsername.text.toString()
             val password: String = inputPassword.text.toString()
             val device: String = Secure.getString(contentResolver, Secure.ANDROID_ID)
-            UtilityStart(this, progressBar).evaluateCreate(username, password, device)
+            evaluateCreate(username, password, device)
         }
 
         this.buttonLogin.setOnClickListener {
@@ -55,12 +77,167 @@ class ActivityStart : AppCompatActivity() {
             val username: String = inputUsername.text.toString()
             val password: String = inputPassword.text.toString()
             val device: String = Secure.getString(contentResolver, Secure.ANDROID_ID)
-            UtilityStart(this, progressBar).requestServer(username, password, device, url)
+            requestServer(username, password, device, url)
         }
 
     }
 
+    fun requestServer(username: String, password: String, device: String, url: String) {
+        progressBar.visibility = View.VISIBLE
 
+        val params = HashMap<String, String>()
+        params["username"] = username
+        params["password"] = password
+        params["device"] = device
+        val jsonObject = JSONObject(params as Map<*, *>)
+
+//uk_f402rtpyd05ucik2myl62m99e
+        Log.e("-request->", "${jsonObject}")
+
+        val request = JsonObjectRequest(
+            Request.Method.POST, url, jsonObject,
+            Response.Listener { response: JSONObject ->
+
+                Log.e("-response->", "${response}")
+
+                this.progressBar.visibility = View.INVISIBLE
+                if (fail(response)) {
+                    return@Listener
+                }
+                val playerSelf: EntityPlayer = parsePlayer.execute(response)
+                this.startActivityHome(playerSelf)
+            },
+            {
+
+                Log.e("-->", "${it}")
+
+                this.progressBar.visibility = View.INVISIBLE
+                val title: String = "⚡ server error ⚡"
+                val message: String = "\uD83D\uDD0C unable to reach server.\ncheck connection and try again. \uD83D\uDCF1"
+                //DialogOk(context).render(title, message)
+                val dialogBuilder = AlertDialog.Builder(applicationContext, R.style.AlertDialog)
+                dialogBuilder.setTitle(title)
+                dialogBuilder.setMessage(message)
+                dialogBuilder.setPositiveButton("ok") { dialog, _ ->
+                    dialog.cancel()
+                }
+                val alert: AlertDialog = dialogBuilder.create()
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                alert.window!!.setType(WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY - 1)
+                }
+                else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                alert.window!!.setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT)
+                }
+                alert.show()
+
+            }
+        )
+        request.retryPolicy = DefaultRetryPolicy(DefaultRetryPolicy.DEFAULT_TIMEOUT_MS, 0, 1F)
+        VolleySingleton.getInstance(applicationContext).addToRequestQueue(request)
+    }
+
+    fun evaluateCreate(username: String, password: String, device: String) {
+        if (startActivityCreate(username, password)) {
+            return
+        }
+        if (!isLettersOrDigits(username)) {
+            val title: String = "⚡ illegal input ⚡"
+            val message: String =
+                "\uD83E\uDD16 username must be alphanumeric.\nplease re-evaluate and try again. \uD83D\uDCF2"
+            //this.dialog.render(title, message)
+            val dialogBuilder = AlertDialog.Builder(applicationContext, R.style.AlertDialog)
+            dialogBuilder.setTitle(title)
+            dialogBuilder.setMessage(message)
+            dialogBuilder.setPositiveButton("ok") { dialog, _ ->
+                dialog.cancel()
+            }
+            val alert: AlertDialog = dialogBuilder.create()
+            //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //alert.window!!.setType(WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY - 1)
+            //}
+            //else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            //alert.window!!.setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT)
+            //}
+            alert.show()
+            return
+        }
+        val url: String = "${ServerAddress().IP}:8080/player/create"
+        this.requestServer(username, password, device, url)
+    }
+
+    private fun isLettersOrDigits(chars: String): Boolean {
+        for (c in chars) {
+            if (c !in 'A'..'Z' && c !in 'a'..'z' && c !in '0'..'9') {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun startActivityCreate(username: String, password: String): Boolean {
+        if (username != "" || password != "") {
+            return false
+        }
+        val intent = Intent(applicationContext, ActivityCreate::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        applicationContext.startActivity(intent)
+        this.finish()
+        return true
+    }
+
+    private fun startActivityHome(player: EntityPlayer) {
+        val extras: ExtendedDataHolder = ExtendedDataHolder().getInstance()
+        extras.putExtra("player_self", player)
+        val intent = Intent(applicationContext, ActivityHome::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        applicationContext.startActivity(intent)
+        this.finish()
+    }
+
+    fun fail(response: JSONObject): Boolean {
+        if (response.has("unknown")) {
+            val title: String = "⚡ unknown username ⚡"
+            val message: String = "\uD83E\uDD16 no such player in database.\nplease re-evaluate and try again. \uD83D\uDCF2"
+            this.render(title, message)
+            return true
+        }
+        if (response.has("invalid")) {
+            val title: String = "⚡ invalid password ⚡"
+            val message: String = "\uD83E\uDD16 input password is incorrect.\nplease re-evaluate and try again. \uD83D\uDCF2"
+            this.render(title, message)
+            return true
+        }
+        if (response.has("reserved")) {
+            val title: String = "⚡ reserved username ⚡"
+            val message: String = "\uD83E\uDD16 input username is reserved.\nplease re-evaluate and try again. \uD83D\uDCF2"
+            this.render(title, message)
+            return true
+        }
+        //connection
+        //illegal
+        return false
+    }
+
+    fun render(title: String, message: String) {
+        val dialogBuilder = AlertDialog.Builder(applicationContext, R.style.AlertDialog)
+        dialogBuilder.setTitle(title)
+        dialogBuilder.setMessage(message)
+        dialogBuilder.setPositiveButton("ok") { dialog, _ ->
+            dialog.cancel()
+        }
+        val alert: AlertDialog = dialogBuilder.create()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            alert.window!!.setType(WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY - 1)
+        }
+        else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            alert.window!!.setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT)
+        }
+        alert.show()
+    }
 }
 
 
