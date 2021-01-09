7bda476958f3bd0b979812d41ef13eca18a25af3
smatthewenglish <s.matthew.english@gmail.com>
2021-01-09 14:44:03 -0500

    2.1.4

--- app/src/main/java/io/tschess/tschess/tschess/ActivityTschess.kt
+++ app/src/main/java/io/tschess/tschess/tschess/ActivityTschess.kt
@@ -2,6 +2,8 @@ package io.tschess.tschess.tschess
 
 import android.app.NotificationManager
 import android.content.Context
+import android.content.DialogInterface
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -11,14 +13,15 @@ import android.view.View
 import android.widget.ProgressBar
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.DefaultRetryPolicy
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.material.tabs.TabLayout
+import com.google.firebase.iid.FirebaseInstanceId
 import io.tschess.tschess.R
-import io.tschess.tschess.dialog.DialogPush
 import io.tschess.tschess.dialog.tschess.DialogDraw
-import io.tschess.tschess.dialog.tschess.DialogOption
 import io.tschess.tschess.dialog.tschess.DialogPromo
 import io.tschess.tschess.header.HeaderSelf
 import io.tschess.tschess.model.EntityGame
@@ -288,7 +291,8 @@ class ActivityTschess : AppCompatActivity(), Listener, Flasher {
                 this.networker.update(this.game.id, state, highlight)
                 /* * */
                 if (this.playerSelf.promptPopup()) {
-                    DialogPush(applicationContext, progressBar).notifications(playerSelf)
+                    //DialogPush(applicationContext, progressBar).notifications(playerSelf)
+                    this.notifications(playerSelf)
                 }
                 /* * */
                 return
@@ -359,6 +363,88 @@ class ActivityTschess : AppCompatActivity(), Listener, Flasher {
         val updatedXX: String = this.game.updated
         val updatedXY: ZonedDateTime = LocalDateTime.parse(updatedXX, this.formatter).atZone(this.brooklyn)
         return updated01.isAfter(updatedXY)
+    }
+
+    fun notifications(player: EntityPlayer) {
+        val dialogBuilder = AlertDialog.Builder(this, R.style.AlertDialog)
+        dialogBuilder.setTitle("☎️ notification ☎")
+        dialogBuilder.setMessage("once your opponent has moved would you like to be notified?")
+            .setPositiveButton("yes \uD83D\uDC4C", DialogInterface.OnClickListener { dialog, id ->
+                val url = "${ServerAddress().IP}:8080/player/push"
+                this.progressBar.visibility = View.VISIBLE
+
+                FirebaseInstanceId.getInstance().instanceId
+                    .addOnCompleteListener(OnCompleteListener { task ->
+                        val token: String? = task.result?.token
+                        var note_key: String = "NULL"
+                        if (!token.isNullOrBlank()) {
+                            note_key = token
+                        }
+                        val params = HashMap<String, String>()
+                        params["id"] = player.id
+                        params["note_key"] = "ANDROID_${note_key}"
+                        val jsonObject = JSONObject(params as Map<*, *>)
+                        //Log.d("jsonObject", "${jsonObject}")
+                        val request = JsonObjectRequest(
+                            Request.Method.POST, url, jsonObject,
+                            { response: JSONObject ->
+                                //Log.d("response", "${response}")
+                                //response
+                                this.progressBar.visibility = View.INVISIBLE
+                            },
+                            {
+                                this.progressBar.visibility = View.INVISIBLE
+                            }
+                        )
+                        request.retryPolicy = DefaultRetryPolicy(DefaultRetryPolicy.DEFAULT_TIMEOUT_MS, 0, 1F)
+                        VolleySingleton.getInstance(this).addToRequestQueue(request)
+                    })
+            })
+            .setNegativeButton("no \uD83D\uDE45\u200D♂️", DialogInterface.OnClickListener { dialog, _ ->
+                //TODO: FUCK
+
+                val url = "${ServerAddress().IP}:8080/player/push"
+                this.progressBar.visibility = View.VISIBLE
+
+                //FirebaseInstanceId.getInstance().instanceId
+                //.addOnCompleteListener(OnCompleteListener { task ->
+                //val token: String? = task.result?.token
+                var note_key: String = "DECLINE"
+                //if (!token.isNullOrBlank()) {
+                //note_key = token
+                //}
+                val params = HashMap<String, String>()
+                params["id"] = player.id
+                params["note_key"] = "ANDROID_${note_key}"
+                val jsonObject = JSONObject(params as Map<*, *>)
+                //Log.d("jsonObject", "${jsonObject}")
+                val request = JsonObjectRequest(
+                    Request.Method.POST, url, jsonObject,
+                    { response: JSONObject ->
+                        //Log.d("response", "${response}")
+                        //response
+                        this.progressBar.visibility = View.INVISIBLE
+                    },
+                    {
+                        this.progressBar.visibility = View.INVISIBLE
+                    }
+                )
+                request.retryPolicy = DefaultRetryPolicy(DefaultRetryPolicy.DEFAULT_TIMEOUT_MS, 0, 1F)
+                VolleySingleton.getInstance(this).addToRequestQueue(request)
+                //})
+
+
+            })
+        val alert: AlertDialog = dialogBuilder.create()
+        //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //alert.window!!.setType(WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY - 1)
+        //}
+        //else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            //alert.window!!.setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT)
+        //}
+        alert.show()
+        alert.getButton(DialogInterface.BUTTON_POSITIVE).setTextColor(Color.WHITE)
+        alert.getButton(DialogInterface.BUTTON_NEGATIVE).setTextColor(Color.WHITE)
     }
 
 }
